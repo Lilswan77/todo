c367f50c02ecf6c406ba97c41c018e819cfc1704
Vincent <vsuen123@gmail.com>
2022-10-26 21:24:30 -0400

    todo

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,41 +1,19 @@
-import { useState } from "react";
-
-const createInitialTodos = () => {
-  const initialTodos = [];
-  return initialTodos;
-};
-
-export const App = () => {
-  const [todos, setTodos] = useState(createInitialTodos());
-  const [text, setText] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
+export const Content = () => {
   return (
-    <div className="parent-box">
-      <div className="content-box">
-        <form onSubmit={handleSubmit}>
-          <input value={text} onChange={(e) => setText(e.target.value)} />
-          <button
-            onClick={() => {
-              setText("");
-              setTodos([{ id: todos.length, text: text }, ...todos]);
-            }}
-          >
-            Add
-          </button>
-        </form>
-        <ul>
-          {todos.map((item) => (
-            <li key={item.id}>{item.text}</li>
-          ))}
-              </ul>
-              <h1>Hello world</h1>
+    <div className='parent-box'>
+      <div className='content-box'>
+
+      </div>
+      <div className='content-box'>
+
+
+      </div>
+      <div className='content-box'>
+
+
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default Content
